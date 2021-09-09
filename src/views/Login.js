cea454c7daefbfee9ea27eac725fb5f99517aa47
Kawern <kawern@gmail.com>
2021-09-09 14:56:49 +0200

    mmh

--- src/views/Login.js
+++ src/views/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
         response_type: "code",
         client_id: process.env.REACT_APP_CLIENT_ID,
         scope: "streaming user-read-email user-read-private user-read-playback-state user-read-currently-playing user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state",
-        redirect_uri: "https://thirsty-goodall-de0bb8.netlify.app/callback", 
+        redirect_uri: "/callback", 
         state: "112345678iknbcxsertyuilkmnbvcdfghjk"
     })
     return ( 
